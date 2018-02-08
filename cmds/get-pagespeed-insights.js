cc2d39454b2feaabdf3859f5d122367809a00f3f
Bryan Pizzillo <bryanp@mail.nih.gov>
2018-02-07 23:23:31 -0500

    Moving to async/await

--- cmds/get-pagespeed-insights.js
+++ cmds/get-pagespeed-insights.js
@@ -17,6 +17,130 @@ let logger = new Logger({
   level: "debug" }
 );
 
+function validateConfig() {
+  /*****************************************
+   * Validate Easy Stuff
+   *****************************************/
+  if (!config.has('server') || (config.get('server') == "")) {
+    throw new Error('Configuration Issue: server is not set')
+  }
+
+  if (!config.has('gapi_key') || (config.get('gapi_key') == "")) {
+    throw new Error('Configuration Issue: gapi_key is not set')
+  }
+
+  if (
+    !config.has("psi_batch_size") ||
+    !config.has("psi_batch_sleep") ||
+    !config.has("psi_max_async") ||
+    !config.has("psi_max_retries") ||
+    !config.has("psi_waittime_between_errors")
+  ) {
+    throw new Error(`Invalid Config File.  Must contain psi_batch_size, psi_batch_sleep and psi_max_async!`);
+  }
+
+  if (!config.has('storage_provider')) {
+    throw new Error('Configuration Issue: storage_provider is not set')
+  }      
+
+  if (!config.has('storage_provider_config')) {
+    throw new Error('Configuration Issue: storage_provider_config is not set')
+  }
+}
+
+async function loadStorageProvider() {
+  /************************************
+   * Load Storage Providers
+   ************************************/
+
+
+  let storageProviderName = config.get('storage_provider');
+  let storageProviderConfig = config.get('storage_provider_config');
+
+  let storageProviderModulePath = path.join(__dirname, '..', 'lib', 'storage-providers', storageProviderName);
+  let storageProviderModule = null;
+
+  //Load the provider Module
+  try {
+    storageProviderModule = require(storageProviderModulePath);
+  } catch(err) {
+    logger.error(`Could not load storage provider, ${storageProviderName}. ${err.message}`);
+    logger.error(err);
+    throw new Error("Could not load storage provider");
+  }
+
+  //Validate the config
+  let configErrors = storageProviderModule.ValidateConfig(storageProviderConfig);
+  if (configErrors.length > 0) {
+    logger.error('Provider Configuration Errors Detected');
+    configErrors.forEach(err => {
+      logger.error(err);
+    });  
+    throw new Error("Invalid Configuration")
+  }
+
+  let storageProvider = await storageProviderModule.GetProviderInstance(logger, storageProviderConfig);
+
+  return storageProvider;
+}
+
+async function action() {
+  // Validate the configuration
+  try {
+    validateConfig();
+  } catch (err) {
+    logger.error(err.message);
+    process.exit(1);
+  }
+  
+
+  //Create an storageProviderInstance
+  let storageProviderInstance = null;
+  try {
+    storageProviderInstance = await loadStorageProvider();
+  } catch (err) {
+    logger.error("Could not load storage provider")
+    process.exit(2);
+  }
+
+  let sitemapExtractor = new SitemapUrlExtractor(
+    logger, 
+    config.get("server"),
+    {}
+  );
+
+  let processor = new InsightsProcessor(
+    logger,            
+    storageProviderInstance,
+    {
+      insightsApiKey: config.get("gapi_key"),
+      psi_batch_size: config.get("psi_batch_size"),
+      psi_batch_sleep: config.get("psi_batch_sleep"),
+      psi_max_async: config.get("psi_max_async"),
+      psi_max_retries: config.get("psi_max_retries"),
+      psi_waittime_between_errors: config.get("psi_waittime_between_errors")
+    }
+  );
+
+  try {
+    //Cal the sitemap extractor THEN the processor's process method (which takes in a list of URLs)
+    //return sitemapExtractor.extractPages()
+    //    .then(processor.process.bind(processor))            
+
+    await processor.process(['https://www.cancer.gov/about-cancer']);
+
+    //Exit
+    logger.info("Finished.  Exiting...")
+    process.exit(0);
+    
+  } catch (err) {
+    logger.error(`Fatal Error Occurred in get-pagespeed-insights.`)                
+    logger.error(err);
+    logger.error("Errors occurred.  Exiting");
+    process.exit(3);
+  }
+}
+
 function GetPageSpeedInsights(program) {
   program
     .command('get-pagespeed-insights')
@@ -27,110 +151,6 @@ function GetPageSpeedInsights(program) {
     ')
     .action((cmd) => {
 
-      /*****************************************
-       * Validate Easy Stuff
-       *****************************************/
-      if (!config.has('server') || (config.get('server') == "")) {
-        logger.error('Configuration Issue: server is not set')
-        process.exit(1);
-      }
-
-      if (!config.has('gapi_key') || (config.get('gapi_key') == "")) {
-        logger.error('Configuration Issue: gapi_key is not set')
-        process.exit(1);
-      }
-
-      if (
-        !config.has("psi_batch_size") ||
-        !config.has("psi_batch_sleep") ||
-        !config.has("psi_max_async") ||
-        !config.has("psi_max_retries") ||
-        !config.has("psi_waittime_between_errors")
-      ) {
-        logger.error(`Invalid Config File.  Must contain psi_batch_size, psi_batch_sleep and psi_max_async!`);
-        process.exit(1);
-      }
-
-      /************************************
-       * Validate and Load Storage Providers
-       ************************************/
-
-      if (!config.has('storage_provider')) {
-        logger.error('Configuration Issue: storage_provider is not set')
-        process.exit(1);
-      }      
-
-      if (!config.has('storage_provider_config')) {
-        logger.error('Configuration Issue: storage_provider_config is not set')
-        process.exit(1);
-      }      
-
-      let storageProviderName = config.get('storage_provider');
-      let storageProviderConfig = config.get('storage_provider_config');
-
-      let storageProviderModulePath = path.join(__dirname, '..', 'lib', 'storage-providers', storageProviderName);
-      let storageProviderModule = null;
-
-      //Load the provider Module
-      try {
-        storageProviderModule = require(storageProviderModulePath);
-      } catch(err) {
-        logger.error(`Could not load storage provider, ${storageProviderName}. ${err.message}`);
-        process.exit(1);
-      }
-      
-      //Validate the config
-      let configErrors = storageProviderModule.ValidateConfig(storageProviderConfig);
-      if (configErrors.length > 0) {
-        logger.error('Configuration Errors Detected');
-        configErrors.forEach(err => {
-          logger.error(err);
-        });  
-        process.exit(1);
-      }
-
-      //Create an storageProviderInstance
-      storageProviderModule.GetProviderInstance(logger, storageProviderConfig)
-        // Then create a processor and get a promise for the processing of the instance
-        .then((storageProviderInstance) => {
-
-          let sitemapExtractor = new SitemapUrlExtractor(
-            logger, 
-            config.get("server"),
-            {}
-          );
-
-          let processor = new InsightsProcessor(
-            logger,            
-            storageProviderInstance,
-            {
-              insightsApiKey: config.get("gapi_key"),
-              psi_batch_size: config.get("psi_batch_size"),
-              psi_batch_sleep: config.get("psi_batch_sleep"),
-              psi_max_async: config.get("psi_max_async"),
-              psi_max_retries: config.get("psi_max_retries"),
-              psi_waittime_between_errors: config.get("psi_waittime_between_errors")
-            }
-          );
-
-          //Cal the sitemap extractor THEN the processor's process method (which takes in a list of URLs)
-          //return sitemapExtractor.extractPages()
-          //    .then(processor.process.bind(processor))            
-          return processor.process(['https://www.cancer.gov/about-cancer']);
-        })
-        // Then when processing is done, exit.
-        .then(() => {          
-          //Exit
-          logger.info("Finished.  Exiting...")
-          process.exit(0);
-        })
-        //Otherwise, catch any fatal error and exit.
-        .catch((err) => {  
-          logger.error(`Fatal Error Occurred in get-pagespeed-insights.`)                
-          logger.error(err);
-          logger.error("Errors occurred.  Exiting");
-          process.exit(2);
-        });
-
+      action();
     });
 }
